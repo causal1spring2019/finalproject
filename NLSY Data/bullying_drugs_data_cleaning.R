13e89bd4275590c5467e72f51be0454d6f254d1d
Veronica <vpear@berkeley.edu>
2019-04-16 17:52:32 -0700

    Update bullying_drugs_data_cleaning.R

--- NLSY Data/bullying_drugs_data_cleaning.R
+++ NLSY Data/bullying_drugs_data_cleaning.R
@@ -27,8 +27,23 @@ final_data = subset(final_data, !is.na(final_data$bullied_bf_12_1997) & !is.na(f
 # save dataset
 save(final_data, file = "~/Documents/GitHub/finalproject/NLSYdata.Rdata")
 
-
 # joint distribution of exposure and outcome
 table(final_data$bullied_bf_12_1997,final_data$ever_new_user2, useNA="ifany")
 table(final_data$bullied_bf_12_1997)
 table(final_data$ever_new_user2)
+
+#### Single imputation for missing values
+load("~/Documents/GitHub/finalproject/NLSY Data/NLSYdata.Rdata")
+library(mice)
+
+#remove vars not used in prediction
+for_imputation = final_data[,3:22]
+
+#Impute data (1 imputed sets)
+MICEdata = mice(for_imputation, seed = 5, m=1)
+imputed_data = complete(MICEdata)
+
+# add ID vars
+imputed_data = cbind(final_data[,1:2],imputed_data)
+
+save(imputed_data, file= "~/Documents/GitHub/finalproject/NLSY Data/imputed_data.Rdata")
